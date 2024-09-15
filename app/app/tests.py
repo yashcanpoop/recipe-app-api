bba7d9a314cca347b8cfe04cbce71586c9003706
yashcanpoop <yashpathania21bca099@gmail.com>
2024-09-15 17:49:22 +0530

    "Removed calc.py file, updated settings.py to use PostgreSQL, and commented out tests in tests.py"

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,15 +1,16 @@
-"""
-Sample test
-"""
-from django.test import SimpleTestCase
+# """
+# Sample test
+# """
+# from django.test import SimpleTestCase
 
-from app import calc
+# from app import calc
 
-class ClassTests(SimpleTestCase):
-    def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
 
-    def test_subtract_numbers(self):
-        res = calc.subtract(10,15)
-        self.assertEqual(res, 5)
+# class ClassTests(SimpleTestCase):
+#     def test_add_numbers(self):
+#         res = calc.add(5, 6)
+#         self.assertEqual(res, 11)
+
+#     def test_subtract_numbers(self):
+#         res = calc.subtract(10, 15)
+#         self.assertEqual(res, 5)
